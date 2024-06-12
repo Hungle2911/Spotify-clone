0bdeb4643072df6af8759f9fb3cdb36d48ad78ea
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-06-11 23:37:54 -0400

    feat: set up route for main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import "./App.css";
 import HomePage from "./routes/HomePage";
+import MainPage from "./routes/MainPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/main" element={<MainPage />} />
       </Routes>
     </>
   );
